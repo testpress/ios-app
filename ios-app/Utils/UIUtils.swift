d91505d854b05b5d805da5fcef3ba6de2d23c953
vignesh-s <vignesh@testpress.in>
2017-05-13 19:01:09 +0530

    Change file hierarchy & order

--- ios-app/Utils/UIUtils.swift
+++ ios-app/Utils/UIUtils.swift
@@ -83,36 +83,3 @@ public class UIUtils {
     }
     
 }
-
-public class Colors {
-    
-    static let PRIMARY = "#2196F3"
-    static let PRIMARY_TEXT = "#ffffff"
-    static let MATERIAL_GREEN = "#00ccaa"
-    static let MATERIAL_RED = "#e65c6c"
-    static let GRAY_LIGHT = "#e6e6e6"
-    static let GRAY_MEDIUM = "#999999"
-    static let TAB_TEXT_COLOR = "#888888"
-
-    static func getRGB (_ hex:String) -> UIColor {
-        var cString:String
-            = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if (cString.hasPrefix("#")) {
-            cString = (cString as NSString).substring(from: 1)
-        }
-        
-        let rString = (cString as NSString).substring(to: 2)
-        let gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
-        let bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
-        
-        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
-        Scanner(string: rString).scanHexInt32(&r)
-        Scanner(string: gString).scanHexInt32(&g)
-        Scanner(string: bString).scanHexInt32(&b)
-        
-        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0,
-                       alpha: CGFloat(1))
-    }
-    
-}
