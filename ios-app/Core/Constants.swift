9027885743223925515c586f6c55376f70016054
vignesh-s <vignesh@testpress.in>
2017-07-25 18:08:55 +0530

    Customise for IITBUDS
    Modified images, name, color, url.

--- ios-app/Core/Constants.swift
+++ ios-app/Core/Constants.swift
@@ -26,11 +26,11 @@
 import Foundation
 
 struct Constants {
-    static let BASE_URL = "https://sandbox.testpress.in";
+    static let BASE_URL = "https://online.iitbuds.com";
     
-    static let APP_APPLE_ID = "1235683818"
+    static let APP_APPLE_ID = "1263044137"
     
-    static let APP_SHARE_MESSAGE = "Good app to prepare for online exams. Get it at http://itunes.apple.com/app/id" + APP_APPLE_ID
+    static let APP_SHARE_MESSAGE = "Good app to prepare for IIT JEE Main & Advance. Get it at http://itunes.apple.com/app/id" + APP_APPLE_ID
     
     static let APP_STORE_LINK = "itms-apps://itunes.apple.com/app/id" + APP_APPLE_ID
     
