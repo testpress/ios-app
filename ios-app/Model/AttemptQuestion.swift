1082ebc4fefc82a2b11b7383c0291e66ea1ead7b
vignesh-s <vignesh@testpress.in>
2018-09-03 14:05:00 +0530

    Fix issue on bookmarking same question in different attempts
    - If same questions bookmarked on two different attempts, options in the
    question displayed twice in bookmark detail. This is due to appending
    options(answers) in same question object twice. This is fixed by
    cloning the question object & appending the options to it.

--- ios-app/Model/AttemptQuestion.swift
+++ ios-app/Model/AttemptQuestion.swift
@@ -42,6 +42,27 @@ public class AttemptQuestion {
     
     public required init?(map: Map) {
     }
+    
+    public init() {
+    }
+    
+    public  func clone() -> AttemptQuestion {
+        let newAttemptItem = AttemptQuestion()
+        newAttemptItem.id = id
+        newAttemptItem.questionHtml = questionHtml
+        newAttemptItem.subject = subject
+        newAttemptItem.subjectId = subjectId
+        newAttemptItem.direction = direction
+        newAttemptItem.directionId = directionId
+        newAttemptItem.explanationHtml = explanationHtml
+        newAttemptItem.type = type
+        newAttemptItem.commentsUrl = commentsUrl
+        newAttemptItem.answers = answers
+        newAttemptItem.answerIds = answerIds
+        newAttemptItem.translationIds = translationIds
+        newAttemptItem.isCaseSensitive = isCaseSensitive
+        return newAttemptItem
+    }
 }
 
 extension AttemptQuestion: TestpressModel {
