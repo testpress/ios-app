6c1b17f9e9fa710df613d72121b98e860702f20e
Karthik Raja <Karthik@testpress.in>
2018-12-17 17:45:01 +0530

    Change post label dynamically from institute settings

--- ios-app/Core/Strings.swift
+++ ios-app/Core/Strings.swift
@@ -135,7 +135,7 @@ struct Strings {
     
     static let RESET_PASSWORD_MAIL_SENT = "We have sent you an email with a link to reset your password.  Please check your email and click the link to continue."
     
-    static let ARTICLES = "Articles"
+    static let ARTICLES = instituteSettings.postsLabel
     
     static let INVALID_ACCESS_CODE = "Invalid Access Code"
     
