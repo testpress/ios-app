8033182691f56e9565e6bcd7b4de852a63cb4089
Karthik Raja <Karthik@testpress.in>
2019-02-13 09:47:12 +0530

    Release version 1.0

--- ios-app/UI/MainMenuTabViewController.swift
+++ ios-app/UI/MainMenuTabViewController.swift
@@ -34,14 +34,12 @@ class MainMenuTabViewController: UITabBarController {
         super.viewDidLoad()
         instituteSettings = DBManager<InstituteSettings>().getResultsFromDB()[0]
         viewControllers?[4].tabBarItem.title = instituteSettings.postsLabel
+        viewControllers?.remove(at: 6) // Access code
+
         if (!instituteSettings.forumEnabled) {
             viewControllers?.remove(at: 5)
         }
-        
-        if (!instituteSettings.showGameFrontend) {
-            viewControllers?.remove(at: 1)
-        }
-        
+    
         if (!instituteSettings.postsEnabled) {
             viewControllers?.remove(at: 4)
         }
@@ -49,6 +47,14 @@ class MainMenuTabViewController: UITabBarController {
         if (!instituteSettings.coursesEnableGamification) {
             viewControllers?.remove(at: 3)
         }
+        
+        if (instituteSettings.showGameFrontend) {
+            viewControllers?.remove(at: 2) // Exams list
+            
+        } else {
+            viewControllers?.remove(at: 1)
+        }
+
         if (UserDefaults.standard.string(forKey: Constants.REGISTER_DEVICE_TOKEN) == "true") {
             let deviceToken = UserDefaults.standard.string(forKey: Constants.DEVICE_TOKEN)
             let fcmToken = UserDefaults.standard.string(forKey: Constants.FCM_TOKEN)
