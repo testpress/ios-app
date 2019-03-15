e506fc0c8552c56f7761c799b7fd9b701962b037
Karthik Raja <Karthik@testpress.in>
2019-03-15 09:25:46 +0530

    Release sure success app

--- ios-app/Model/InstituteSettings.swift
+++ ios-app/Model/InstituteSettings.swift
@@ -53,6 +53,7 @@ class InstituteSettings: DBModel {
     @objc dynamic var bookmarksEnabled: Bool = false
     @objc dynamic var forumEnabled: Bool = false
     @objc dynamic var twilioEnabled: Bool = false
+    @objc dynamic var activityFeedEnabled: Bool = false
     
     public override func mapping(map: Map) {
         verificationMethod <- map["verification_method"]
@@ -78,6 +79,7 @@ class InstituteSettings: DBModel {
         bookmarksEnabled <- map["bookmarks_enabled"]
         forumEnabled <- map["forum_enabled"]
         twilioEnabled <- map["twilio_enabled"]
+        activityFeedEnabled <- map["activity_feed_enabled"]
     }
     
     override public static func primaryKey() -> String? {
