5529f54c86353f20d01e3d4d573386c108089cde
Karthik <karthik@testpress.in>
2019-09-09 11:16:42 +0530

    Display alert for max login exceeded error

--- ios-app/Core/Strings.swift
+++ ios-app/Core/Strings.swift
@@ -184,5 +184,7 @@ struct Strings {
     static let CASE_SENSITIVE = "Answers are case sensitive"
     
     static let PARALLEL_LOGIN_RESTRICTION_INFO = "Note : Admin has restricted parallel logged in devices to "
+    static let ACCOUNT_LOCKED = "Account Locked"
+    static let MAX_LOGIN_EXCEEDED_ERROR_MESSAGE = "Your account is locked due to exceed of maximum login limits. Your account will automatically get unlocked within 24 hours."
     
 }
