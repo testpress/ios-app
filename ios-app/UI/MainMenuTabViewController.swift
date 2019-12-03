b82852a140f3ee9a631a7c6d174612b9c7bdf072
Karthik <karthik@testpress.in>
2019-12-03 12:53:42 +0530

    Refactor code

--- ios-app/UI/MainMenuTabViewController.swift
+++ ios-app/UI/MainMenuTabViewController.swift
@@ -54,9 +54,9 @@ class MainMenuTabViewController: UITabBarController {
             viewControllers?.remove(at: 1)
         }
         
-//        if (!instituteSettings.activityFeedEnabled) {
-//            viewControllers?.remove(at: 0)
-//        }
+        if (!instituteSettings.activityFeedEnabled) {
+            viewControllers?.remove(at: 0)
+        }
 
         if (UserDefaults.standard.string(forKey: Constants.REGISTER_DEVICE_TOKEN) == "true") {
             let deviceToken = UserDefaults.standard.string(forKey: Constants.DEVICE_TOKEN)
