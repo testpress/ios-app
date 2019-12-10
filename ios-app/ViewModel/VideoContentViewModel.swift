017fca2abf84530160e55421ba733c9d87a6f92c
Karthik <karthik@testpress.in>
2019-12-10 07:35:48 +0530

    Add test case

--- ios-app/ViewModel/VideoContentViewModel.swift
+++ ios-app/ViewModel/VideoContentViewModel.swift
@@ -97,7 +97,6 @@ class VideoContentViewModel {
                     ).show()
                 
                 completion?()
-//                self.udpateBookmarkButtonState(bookmarkId: nil)
         })
     }
     
