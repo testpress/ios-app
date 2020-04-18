62831bb262992858138f66f7466a85bba2e44b00
Karthik <karthik@testpress.in>
2020-04-18 19:42:02 +0530

    Release version 1.0

--- ios-app/Core/Constants.swift
+++ ios-app/Core/Constants.swift
@@ -26,7 +26,7 @@
 import Foundation
 
 struct Constants {
-    static let BASE_URL = "https://brilliantpala2.testpress.in";
+    static let BASE_URL = "https://brilliantpalaclasses.testpress.in";
     static let APP_APPLE_ID = "1502940452"
     
     static let APP_SHARE_MESSAGE = "Good app to prepare for online exams. Get it at http://itunes.apple.com/app/id" + APP_APPLE_ID
