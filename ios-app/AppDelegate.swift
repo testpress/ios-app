643f99af42b3a77149876f1fa9f9fc37addd6bf4
Karthik raja <41418580+Karthik-0@users.noreply.github.com>
2020-10-08 15:22:58 +0530

    Add view controller to initialise and join zoom meeting (#48)
    - Added ZoomMeetView controller which supports initializing and joining to zoom meeting

--- ios-app/AppDelegate.swift
+++ ios-app/AppDelegate.swift
@@ -136,6 +136,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
+        Zoom.enableFullScreenForMeetingWaitView()
         return true
     }
     
