678bdd8c7ca52faa2629ecbaf6f4d3cfaa6453c1
Karthik raja <karthik@testpress.in>
2021-12-29 09:54:04 +0530

    Display accepted answer in discussion (#70)
    - Display accepted answer in discussions

--- ios-app/UI/PostDetailViewController.swift
+++ ios-app/UI/PostDetailViewController.swift
@@ -114,7 +114,7 @@ class PostDetailViewController: BaseWebViewController, WKWebViewDelegate, WKScri
                 self.loading = false
                 self.post = post
                 self.webView.loadHTMLString(
-                    self.getFormattedContent(post!.contentHtml!),
+                    self.getFormattedContent(post),
                     baseURL: Bundle.main.bundleURL
                 )
         })
@@ -317,9 +317,9 @@ class PostDetailViewController: BaseWebViewController, WKWebViewDelegate, WKScri
         return WebViewUtils.getFormattedTitle(title: post.title)
     }
     
-    func getFormattedContent(_ contentHtml: String) -> String {
+    func getFormattedContent(_ post: Post?) -> String {
         var html = WebViewUtils.getHeader() + getTitle() +
-            WebViewUtils.getHtmlContentWithMargin(contentHtml)
+            WebViewUtils.getHtmlContentWithMargin(post?.contentHtml ?? "")
         
         html += "<hr style='margin-top:20px;'>"
         html += WebViewUtils.getCommentHeadingTags(headingText: Strings.COMMENTS);
