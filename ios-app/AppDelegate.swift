d1105d5a8ba2f57a350a426e841e1a88c219c29a
hari-testpress <hari@testpress.in>
2022-10-12 20:41:38 +0530

    Release update

--- ios-app/AppDelegate.swift
+++ ios-app/AppDelegate.swift
@@ -136,7 +136,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
-//        Zoom.enableFullScreenForMeetingWaitView()
+          Zoom.enableFullScreenForMeetingWaitView()
         return true
     }
     
