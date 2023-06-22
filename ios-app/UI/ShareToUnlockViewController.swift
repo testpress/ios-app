cbe7dbd28bbed9ad5a848d0ca161bbf67d7dc592
Prithuvi <prithuvi@Prithuvis-MacBook-Pro.local>
2023-06-22 14:01:09 +0530

    fix_app_crash

--- ios-app/UI/ShareToUnlockViewController.swift
+++ ios-app/UI/ShareToUnlockViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ShareToUnlockViewController: UIViewController {
     var shareText: String = ""
     var onShareCompletion: (() -> Void)?
+    var activityViewController: UIActivityViewController? = nil
     
     @IBOutlet weak var shareButton: UIButton!
     
@@ -25,8 +26,13 @@ class ShareToUnlockViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        setUpPopover()
+    }
+    
     @IBAction func share(_ sender: Any) {
-        let activityViewController = UIActivityViewController(activityItems:
+        activityViewController = UIActivityViewController(activityItems:
             [shareText], applicationActivities: nil)
         let excludeActivities = [
             UIActivity.ActivityType.print,
@@ -38,16 +44,26 @@ class ShareToUnlockViewController: UIViewController {
             UIActivity.ActivityType.airDrop,
             UIActivity.ActivityType.copyToPasteboard
         ]
-        activityViewController.excludedActivityTypes = excludeActivities;
-        
-        activityViewController.completionWithItemsHandler = {(activityType:
+        activityViewController?.excludedActivityTypes = excludeActivities;
+        setUpPopover()
+        activityViewController?.completionWithItemsHandler = {(activityType:
             UIActivity.ActivityType?, completed: Bool, returnedItems:[Any]?, error: Error?) in
             if (completed) {
                 self.onShareCompletion?()
             }
         }
-        self.present(activityViewController, animated: true,
-                     completion: nil)
-        
+        self.present(activityViewController!, animated: true, completion: nil)
     }
+
+    func setUpPopover() {
+        if let popoverPresentationController = activityViewController?.popoverPresentationController {
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.permittedArrowDirections = []
+            let screenWidth = UIScreen.main.bounds.width
+            let screenHeight = UIScreen.main.bounds.height
+            let centerPoint = CGPoint(x: screenWidth / 2, y: screenHeight / 2)
+            popoverPresentationController.sourceRect = CGRect(origin: centerPoint, size: .zero)
+        }
+    }
+    
 }
