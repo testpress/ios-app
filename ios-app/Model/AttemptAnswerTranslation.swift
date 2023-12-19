a43e66b134f54725e3cdaf217450301a360e7307
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-12-19 15:06:45 +0530

    fix

--- ios-app/Model/AttemptAnswerTranslation.swift
+++ ios-app/Model/AttemptAnswerTranslation.swift
@@ -10,12 +10,10 @@ import ObjectMapper
 
 class AttemptAnswerTranslation: DBModel {
     @objc dynamic var textHtml: String = ""
-    @objc dynamic var isCorrect: Bool = false
     
     public override func mapping(map: Map) {
         textHtml <- map["text_html"]
         id <- map["id"]
-        isCorrect <- map["is_correct"]
     }
     
     override public static func primaryKey() -> String? {
