3a79d90a31e98070df64607033b65f18849d9a2c
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-12-21 11:35:32 +0530

    fix

--- ios-app/Model/AttemptQuestionTranslation.swift
+++ ios-app/Model/AttemptQuestionTranslation.swift
@@ -25,7 +25,7 @@ class AttemptQuestionTranslation: DBModel {
         id <- map["id"]
         questionHtml <- map["question_html"]
         direction <- map["direction"]
-        explanationHtml <- map["explanation_html"]
+        explanationHtml <- map["explanation"]
         answers <- (map["answers"], ListTransform<AttemptAnswerTranslation>())
         language <- map["language"]
     }
