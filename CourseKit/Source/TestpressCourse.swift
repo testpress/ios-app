dd0fabe43617b3726e407fbac1a1a90a4ba9fb83
AswinRaaj R I <aswinraaj26@gmail.com>
2024-09-26 16:38:34 +0530

    temp

--- CourseKit/Source/TestpressCourse.swift
+++ CourseKit/Source/TestpressCourse.swift
@@ -28,7 +28,12 @@ public class TestpressCourse {
         print(storyboard)
         print("=========================instatitate end============")
         print(storyboard.instantiateViewController(withIdentifier: identifier))
-        return storyboard.instantiateViewController(withIdentifier: identifier) as? T
+        if let viewController = storyboard.instantiateViewController(withIdentifier: identifier) as? T {
+            return viewController
+        } else {
+            print("Error: Could not instantiate view controller with identifier \(identifier)")
+            return nil
+        }
     }
 
     private func presentViewController(_ viewController: UIViewController?, from context: UIViewController) {
