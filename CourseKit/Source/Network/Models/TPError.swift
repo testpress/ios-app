6079f04765c82c6a405947f8bd62ede85af03280
hari-testpress <hari@testpress.in>
2024-10-10 16:36:15 +0530

    refactor: Move TPError extension logic to core class

--- CourseKit/Source/Network/Models/TPError.swift
+++ CourseKit/Source/Network/Models/TPError.swift
@@ -113,6 +113,28 @@ public class TPError: Error {
         
         SentrySDK.capture(event: event)
     }
+    
+    public func getDisplayInfo() -> (image: UIImage, title: String, description: String) {
+        switch (kind) {
+        case .network:
+            return (Images.TestpressNoWifi.image,
+                    Strings.NETWORK_ERROR,
+                    Strings.PLEASE_CHECK_INTERNET_CONNECTION)
+            
+        case .unauthenticated:
+            return (Images.TestpressAlertWarning.image,
+                    Strings.AUTHENTICATION_FAILED,
+                    Strings.PLEASE_LOGIN)
+        case .custom:
+            return (Images.TestpressAlertWarning.image,
+                    Strings.LOADING_FAILED,
+                    self.error_detail!)
+        default:
+            return (Images.TestpressAlertWarning.image,
+                    Strings.LOADING_FAILED,
+                    Strings.SOMETHIGN_WENT_WRONG)
+        }
+    }
 }
 
 
