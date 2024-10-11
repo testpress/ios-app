98aaa58df0e45fddfb192aee1a62a3ccb9852777
hari-testpress <hari@testpress.in>
2024-10-11 11:47:57 +0530

    refactor: Move UILabel extension to SDK

--- ios-app/UI/Dashboard/CarouselCellViews/DashboardSectionHeaderCell.swift
+++ ios-app/UI/Dashboard/CarouselCellViews/DashboardSectionHeaderCell.swift
@@ -16,20 +16,3 @@ class DashboardSectionHeaderCell: UICollectionViewCell {
     }
 
 }
-
-
-extension UILabel {
-    func addLeading(image: UIImage, text:String) {
-        let attachment = NSTextAttachment()
-        attachment.image = image
-        attachment.bounds = CGRect(x: 0, y: (self.font.capHeight - image.size.height).rounded() / 2, width: image.size.width, height: image.size.height)
-
-        let attachmentString = NSAttributedString(attachment: attachment)
-        let mutableAttributedString = NSMutableAttributedString()
-        mutableAttributedString.append(attachmentString)
-        
-        let string = NSMutableAttributedString(string: "  " + text, attributes: [:])
-        mutableAttributedString.append(string)
-        self.attributedText = mutableAttributedString
-    }
-}
