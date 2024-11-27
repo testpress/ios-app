67f62c55d9428bc78d399a35eae1d6eb3c241c2b
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-11-27 11:29:11 +0530

    fix

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     .package(url: "https://github.com/xmartlabs/XLPagerTabStrip", .upToNextMajor(from: "9.1.0")),
     .package(url: "https://github.com/airbnb/lottie-ios", .upToNextMajor(from: "4.4.3")),
     .package(url: "https://github.com/LaurentiuUngur/LUExpandableTableView", .upToNextMajor(from: "6.0.0")),
-    .package(url: "https://github.com/testpress/iOSPlayerSDK", exact: "1.1.9"),
+    .package(url: "https://github.com/testpress/iOSPlayerSDK", branch: "latest_without_sentry"),
   ],
 
 
