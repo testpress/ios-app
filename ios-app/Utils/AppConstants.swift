563c4514829336043f648181e6d222dec219b39b
Nivethaa-Testpress <nivethaad@testpress.in>
2025-05-13 17:33:17 +0530

    Initial release version 3.0.3

--- ios-app/Utils/AppConstants.swift
+++ ios-app/Utils/AppConstants.swift
@@ -10,9 +10,9 @@ import Foundation
 
 public struct AppConstants {
     
-    public static let SUBDOMAIN = "lmsdemo"
-    public static let APP_APPLE_ID = "1434052944"
+    public static let SUBDOMAIN = "inspireiasacademy"
+    public static let APP_APPLE_ID = "6745190498"
     public static let APP_SHARE_MESSAGE = "Good app to prepare for online exams. Get it at http://itunes.apple.com/app/id" + APP_APPLE_ID
     public static let APP_STORE_LINK = "itms-apps://itunes.apple.com/app/id" + APP_APPLE_ID
-    public static let PRIMARY_COLOR = "#2196F3"
+    public static let PRIMARY_COLOR = "#251c75"
 }
