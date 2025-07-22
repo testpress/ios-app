3cef684e2d982b9bd77ee2661a2d458c29a3631d
pruthiviraj <pruthiviraj@testpress.in>
2025-07-22 18:48:08 +0530

    Initial release version 3.0.9

--- ios-app/Utils/AppConstants.swift
+++ ios-app/Utils/AppConstants.swift
@@ -10,8 +10,8 @@ import Foundation
 
 public struct AppConstants {
     
-    public static let SUBDOMAIN = "lmsdemo"
-    public static let APP_APPLE_ID = "1434052944"
+    public static let SUBDOMAIN = "password"
+    public static let APP_APPLE_ID = "6748629879"
     public static let APP_SHARE_MESSAGE = "Good app to prepare for online exams. Get it at http://itunes.apple.com/app/id" + APP_APPLE_ID
     public static let APP_STORE_LINK = "itms-apps://itunes.apple.com/app/id" + APP_APPLE_ID
     public static let PRIMARY_COLOR = "#2196F3"
