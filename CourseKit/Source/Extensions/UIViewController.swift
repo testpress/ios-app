aef3238eb19bd699ec9ab532ab4ab885b8536628
balamurugan-testpress <balamurugan@testpress.in>
2025-11-20 19:03:22 +0530

    Fix Dynamic Island Gap on iPhone 14 Pro and Later

--- CourseKit/Source/Extensions/UIViewController.swift
+++ CourseKit/Source/Extensions/UIViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 extension UIViewController {
     public func setStatusBarColor() {
         if #available(iOS 13.0, *) {
-                let app = UIApplication.shared
-                let statusBarHeight: CGFloat = app.statusBarFrame.size.height
-                let statusbarView = UIView()
-                statusbarView.backgroundColor = TestpressCourse.shared.statusBarColor
-                view.addSubview(statusbarView)
-                statusbarView.translatesAutoresizingMaskIntoConstraints = false
-                statusbarView.heightAnchor
-            .constraint(equalToConstant: statusBarHeight).isActive = true
-                statusbarView.widthAnchor
-            .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-                statusbarView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-                statusbarView.centerXAnchor.constraint(equalTo:   view.centerXAnchor).isActive = true
+            view.subviews.filter { $0.tag == 999999 }.forEach { $0.removeFromSuperview() }
+            let statusbarView = UIView()
+            statusbarView.backgroundColor = TestpressCourse.shared.statusBarColor
+            statusbarView.tag = 999999
+            view.addSubview(statusbarView)
+            statusbarView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                    statusbarView.topAnchor.constraint(equalTo: view.topAnchor),
+                    statusbarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    statusbarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    statusbarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+                ])
           } else {
               let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar")
                   as? UIView
