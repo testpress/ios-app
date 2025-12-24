b13ebc4d38c2fa76a50315b63ae239989172e9f5
balamurugan-testpress <balamurugan@testpress.in>
2025-12-24 12:39:30 +0530

    chore: expose TPStreams SDK dependency for CourseKit

--- Package.swift
+++ Package.swift
@@ -28,6 +28,7 @@ let package = Package(
     .package(url: "https://github.com/xmartlabs/XLPagerTabStrip", .upToNextMajor(from: "9.1.0")),
     .package(url: "https://github.com/airbnb/lottie-ios", .upToNextMajor(from: "4.4.3")),
     .package(url: "https://github.com/LaurentiuUngur/LUExpandableTableView", .upToNextMajor(from: "6.0.0")),
+    .package(url: "https://github.com/testpress/iOSPlayerSDK", exact: "1.2.10")
   ],
 
 
@@ -51,6 +52,7 @@ let package = Package(
         "IGListKit",
         "XLPagerTabStrip",
         .product(name: "Lottie", package: "lottie-ios"),
+        .product(name: "TPStreamsSDK", package: "iOSPlayerSDK"),
       ],
       path: "CourseKit/Source",
       resources: [
