63b5fbf2c0c83cbed946eaf9f284decb8ed168d3
Balamurugan-Testpress <balamurugan@testpress.in>
2026-03-24 15:37:52 +0530

    refactor: rewrite the function with regex

--- ios-app/Utils/DeepLinkRouter.swift
+++ ios-app/Utils/DeepLinkRouter.swift
@@ -6,17 +6,6 @@ public class DeepLinkRouter {
     public static let shared = DeepLinkRouter()
     private init() {}
 
-    public func getViewController(for url: URL) -> UIViewController? {
-        guard let contentID = extractContentID(from: url), let id = Int(contentID) else { return nil }
-        
-        guard let vc = UIStoryboard(name: Constants.CHAPTER_CONTENT_STORYBOARD, bundle: TestpressCourse.bundle)
-            .instantiateViewController(withIdentifier: Constants.CONTENT_DETAIL_PAGE_VIEW_CONTROLLER) as? ContentDetailPageViewController
-        else { return nil }
-        
-        vc.contentId = id
-        return vc
-    }
-
     public func route(url: URL) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
@@ -32,10 +21,18 @@ public class DeepLinkRouter {
         }
     }
 
+    public func getViewController(for url: URL) -> UIViewController? {
+        guard let contentID = extractContentID(from: url), let id = Int(contentID) else { return nil }
+        
+        guard let vc = UIStoryboard(name: Constants.CHAPTER_CONTENT_STORYBOARD, bundle: TestpressCourse.bundle)
+            .instantiateViewController(withIdentifier: Constants.CONTENT_DETAIL_PAGE_VIEW_CONTROLLER) as? ContentDetailPageViewController
+        else { return nil }
+        
+        vc.contentId = id
+        return vc
+    }
+
     private func extractContentID(from url: URL) -> String? {
-        if let range = url.path.range(of: "(?<=/contents/)[0-9]+", options: .regularExpression) {
-            return String(url.path[range])
-        }
-        return nil
+        return url.path.range(of: "(?<=/contents/)[0-9]+", options: .regularExpression).map { String(url.path[$0]) }
     }
 }
